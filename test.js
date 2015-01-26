fb72655d4cc5eadd67b820ad9edc6a16399f8bd5
Remco <remco@dutchcoders.io>
2015-01-26 13:21:58 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,10 +2,11 @@ function main(a, b) {
   switch (cond) {
   case "one":
     doSomething(); // JSHint will warn about missing 'break' here.
+  case "three":
+    doSomethingElse();
   case "two":
     doSomethingElse();
   }
-
 
   return a == null;
 }
