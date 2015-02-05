6f655c638b44b36dc85f1a53bac2fd508f05006e
Michał Nowak <minow1980@gmail.com>
2015-02-05 01:25:13 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
-test = prompt("name","jan")
-alert(test);
+function main(a, b) {
+  switch (cond) {
+  case "one":
+    doSomething(); // JSHint will warn about missing 'break' here.
+  case "three":
+    doSomethingElse();
+  case "two":
+
+    doSomethingElse();
+  }
+
+// test for deleting old comments. 
+
+
+  return a == null;
+}
