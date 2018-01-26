4eff059b49fdd0aae80fdf6ae07ac048cfc89337
mujahidhemanitw <mujahid.hemani@thoughtwire.ca>
2018-01-26 15:38:58 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+function main(a, b) {
+  switch (cond) {
+  case "one":
+    doSomething(); // JSHint will warn about missing 'break' here.
+  case "three":
+    doSomethingElse();
+  case "two":
+    
+    doSomethingElse();
+  }
+  
+// test for deleting old comments. 
+
+
+  return a == null;
+}
